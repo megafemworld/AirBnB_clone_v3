f82a2735638334e7778ecad75edf1baed5231d11
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-04 23:34:16 +0100

    Femi branch

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" API MAIN """
+
+from flask import Flask, Blueprint
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def tear_down_appcontext():
+    """tear down app context"""
+    storage.close()
+
+if __main__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
