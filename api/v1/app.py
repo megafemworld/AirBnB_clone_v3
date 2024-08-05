311232652909b8fcc60ae4ec68b2a94750b81914
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-05 22:41:37 +0100

    cities

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ API MAIN handles connection """
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -16,6 +16,9 @@ def tear_down_appcontext():
     """tear down app context"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}))
 
 if __main__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
