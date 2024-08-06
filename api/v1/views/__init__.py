15bf8e7521f99901e7ebb6ffb37aa645ff3f16a7
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-06 14:11:52 +0100

    api init 1

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
 """Initialize Blueprint views"""
+
+from flask import Blueprint
+
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.users import *
 from api.v1.views.states import *
@@ -8,7 +10,6 @@ from api.v1.views.places_amenities import *
 from api.v1.views.places import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
-from flask import Blueprint
 from models.state import State
 from models.city import City
 from models import storage
